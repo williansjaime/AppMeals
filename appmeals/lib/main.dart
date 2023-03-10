39ae60bba0e949fc4814416c22b0f4e7bf6ac44b
williansjaime <williansjaime@Outlook.com>
2023-03-10 06:39:45 -0300

    Montagen de category itens

--- appmeals/lib/main.dart
+++ appmeals/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
- 
+import 'screens/categoies_screen.dart'; 
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
--- appmeals/lib/screens/categoies_screen.dart
+++ appmeals/lib/screens/categoies_screen.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      maxCrossAxisExtent: 200,
-      childAspectRatio: 3/2,
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 20,      
-      ),
-      children: <Widget>[
-
-      ],
-      );  }
+    return Scaffold(
+      appBar: AppBar(title: Text("Vamos Cozinhar?"),),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3/2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,      
+        ),
+        children: DUMMY_CATEGORIES.map((cat)
+        {
+          return CategoryItem(cat);
+        }).toList(),
+        ),
+    );  }
 }
