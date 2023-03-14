613bfe1f596ba525741ffa604d186aa4c807cb45
williansjaime <williansjaime@Outlook.com>
2023-03-14 08:51:27 -0300

    Melhorias no tema e fontes

--- appmeals/lib/screens/categoies_screen.dart
+++ appmeals/lib/screens/categoies_screen.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("Vamos Cozinhar?"),),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3/2,
