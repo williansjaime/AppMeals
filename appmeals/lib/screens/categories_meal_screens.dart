c4721627bf1dc87a8c013bd44da7c8d0489e3a17
williansjaime <williansjaime@Outlook.com>
2023-03-14 13:21:17 -0300

    melhoria das cores

---
+++ appmeals/lib/screens/categories_meal_screens.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class CategoriesMealsScreen extends StatelessWidget {
+  //const CategoriesMealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Receitas"),
+      ),
+      body: Center(
+        child: Text("Receitas por Categoria"),
+        ),
+    );
+  }
+}
